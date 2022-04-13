4fe1403c4ec0940432a5c80e607892873fc83323
Octavio Frangipani <62032399+octapf@users.noreply.github.com>
2022-04-13 21:22:03 +0200

    add navBar component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import MyNavbar from './MyNavbar'
 
 const Header = () => {
-	return <header>Header</header>
+	return (
+		<header>
+			<MyNavbar />
+		</header>
+	)
 }
 
 export default Header
